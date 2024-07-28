6d4477d44dbe0c7d483192162687d7e0b9c6705b
oliwier <olik03.2003@wp.pl>
2024-07-28 13:56:38 +0200

    fetching data from json

--- frontend/src/page/main/components/BrandDescription.tsx
+++ frontend/src/page/main/components/BrandDescription.tsx
@@ -1,28 +1,16 @@
+import desc from "../data/brandDescription.json";
+
 const BrandDescription = () => (
   <>
     <h3 className="text-primary font-bold text-2xl sm:text-5xl">
-      Motorcycle Training Courses
+      {desc.mainTitle}
     </h3>
     <h5 className="mt-5 font-bold text-black text-base sm:text-lg">
-      These are the only courses in Poland conducted under the supervision of
-      top specialists.
+      {desc.subTitle}
     </h5>
     <div className="mt-2 text-black max-w-4xl text-sm sm:text-base">
-      <p>
-        Our motorcycle training courses in Poland are designed to teach you how
-        to ride safely and maximize your enjoyment on two wheels. We offer
-        comprehensive training sessions in the cities of Warsaw, Gdańsk, and
-        Kraków, utilizing the most popular tracks in these locations. Our
-        instructors are dedicated to helping students achieve their motorcycle
-        training goals quickly, understand road safety, perceive hazards, and
-        build upon any existing motorcycling skills they possess.
-      </p>
-      <p className="mt-3">
-        In addition to training for your motorcycle license, we provide sessions
-        that give you a taste of the two-wheeled lifestyle. We also offer a wide
-        range of motorcycles available for rent on training days, or you can
-        choose to bring your own bike for the sessions.
-      </p>
+      <p>{desc.firstText}</p>
+      <p className="mt-3">{desc.secondText}</p>
     </div>
     <div className="flex flex-col justify-center items-center text-black mt-2">
       <p>We're in:</p>
--- frontend/src/page/main/components/StoryCards.tsx
+++ frontend/src/page/main/components/StoryCards.tsx
@@ -1,9 +1,11 @@
+import storyCardsData from "../data/storyCards.json";
+
 interface ImageWithDescriptionProps {
   imgSrc: string;
   description: string;
 }
 
-const HistoryCard: React.FC<ImageWithDescriptionProps> = ({
+const StroyCard: React.FC<ImageWithDescriptionProps> = ({
   description,
   imgSrc,
 }) => {
@@ -22,29 +24,10 @@ const HistoryCard: React.FC<ImageWithDescriptionProps> = ({
 };
 
 const StoryCards = () => {
-  const historyCards = [
-    {
-      imgSrc:
-        "https://i.pinimg.com/564x/ce/50/f9/ce50f9070df3946329f6ee0af12e5678.jpg",
-      description:
-        "We're organizing motocross events and competitions every year.",
-    },
-    {
-      imgSrc:
-        "https://i.pinimg.com/564x/7c/ce/db/7ccedbe42077f2257346640faae95ff6.jpg",
-      description: "Our team is same passionate about automotive as you are.",
-    },
-    {
-      imgSrc:
-        "https://i.pinimg.com/originals/03/74/a8/0374a84b0a883b56e2a0af550efbac09.jpg",
-      description:
-        "We value safety over appearance. Unless you come in a suit...",
-    },
-  ];
   return (
     <div className="mt-40 flex justify-center space-x-4">
-      {historyCards.map((card, index) => (
-        <HistoryCard
+      {storyCardsData.map((card, index) => (
+        <StroyCard
           key={index}
           imgSrc={card.imgSrc}
           description={card.description}
