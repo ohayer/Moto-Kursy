a4982e4c0e7df6c23b38ca8b31998d2961889715
oliwier <olik03.2003@wp.pl>
2024-07-29 08:51:19 +0200

    routing paths

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./page/main/MainPage";
+import LoginPage from "./page/adminPanel/LoginPage";
+import DefaultPage from "./page/notFound/DefaultPage";
 
 function App() {
-  return <MainPage />;
+  return (
+    <div className="bg-wmain">
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/admin" element={<LoginPage />} />
+          <Route path="*" element={<DefaultPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/page/notFound/DefaultPage.tsx
@@ -0,0 +1,10 @@
+const DefaultPage = () => (
+  <div className="w-full h-full place-content-center text-black">
+    <h1 className="text-9xl">404</h1>
+    <p className="text-xl mt-7">
+      This page tab does not exist yet or we are still working on it.
+    </p>
+  </div>
+);
+
+export default DefaultPage;
